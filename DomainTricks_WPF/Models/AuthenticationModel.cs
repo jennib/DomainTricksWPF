7bf65d688dd62f7f67a8ffa9bb9ed2dd94e9b916
Jennifer Beidas <jenni.b@gmail.com>
2022-07-16 18:42:41 -0400

    Add AuthenticationModel Class
    Class to hold data for user authentication.

---
+++ DomainTricks_WPF/Models/AuthenticationModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainTricks_WPF.Models
+{
+    public class AuthenticationModel
+    {
+        public string? DomainName  { get; set; }
+        public string? UserNaem { get; set; }
+        public string? Password { get; set; }
+        
+    }
+}
